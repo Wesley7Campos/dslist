0ff0f4740b1e16e24760649fc5862f5f82f34fb8
Wesley Campos <wesley.camposml@gmail.com>
2024-11-11 22:30:22 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -3,7 +3,6 @@ package com.devsuperior.dslist.entities;
 import jakarta.persistence.*;
 
 import java.util.Objects;
-import java.util.UUID;
 
 @Entity
 @Table(name = "tb_game ")
@@ -12,25 +11,29 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
-
     }
 
-    public Game(String genre, Long id, String imgUrl, String longDescription, String platform, Double score, String shortDescription, String title, Integer year) {
+    public Game(String genre, Long id, String imgUrl, String longDescription, String platforms, Double score, String shortDescription, String title, Integer year) {
         this.genre = genre;
         this.id = id;
         this.imgUrl = imgUrl;
         this.longDescription = longDescription;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.shortDescription = shortDescription;
         this.title = title;
@@ -69,12 +72,12 @@ public class Game {
         this.longDescription = longDescription;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
